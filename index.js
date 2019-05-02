67a6c356a0415f12a812bed1f6141eec4d751342
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 18:41:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,5 +61,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber) {
     var message = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+    
   }
 }
