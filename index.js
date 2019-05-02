a18c6ad031b07951e2c6e4ad2a5f5dcfc45d388c
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 18:08:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var c = getCart();
   console.log(c);
-  setCart(getCart().push({itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1}));
+  setCart(c.push({itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1}));
   console.log(getCart());
   return `${item} has been added to your cart.`
 }
